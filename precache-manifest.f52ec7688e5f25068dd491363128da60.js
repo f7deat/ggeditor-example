fc07644c2bd4eb5b78db447e0b531f3d5aa7e4a6
TanDC_VM <tandc_vm@novaon.dc>
2020-10-02 14:45:51 +0700

    Updates

---
+++ precache-manifest.f52ec7688e5f25068dd491363128da60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d8064c20f455aaa39ed92b4d558708b",
+    "url": "/ggeditor-example/index.html"
+  },
+  {
+    "revision": "bf34a149007a642ddff1",
+    "url": "/ggeditor-example/static/css/main.aa5742fb.chunk.css"
+  },
+  {
+    "revision": "09c8da652906e4adb913",
+    "url": "/ggeditor-example/static/js/2.c75c029c.chunk.js"
+  },
+  {
+    "revision": "81fba7bc72c7eb3cb6a2beac8cbb0a53",
+    "url": "/ggeditor-example/static/js/2.c75c029c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf34a149007a642ddff1",
+    "url": "/ggeditor-example/static/js/main.a2f26627.chunk.js"
+  },
+  {
+    "revision": "d62915dcf5816099aea4",
+    "url": "/ggeditor-example/static/js/runtime-main.48e02546.js"
+  }
+]);
