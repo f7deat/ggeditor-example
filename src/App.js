391106c855db99d32d36b8564a73ac5c2e571561
f7deat <tan.dct9x@gmail.com>
2022-02-15 15:55:23 +0700

    pha do

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
 import React from 'react';
 import './App.css';
 import GGEditor, { Flow, EditableLabel } from 'gg-editor';
-import { NodePanel } from './Panel';
-import CustomNode from './CustomNode';
 import Tool from './tools/Tool';
+import data from './data.json'
 
-const data = {
-  nodes: [
-    {
-      id: '0',
-      label: 'Node',
-      x: 55,
-      y: 55,
-    },
-    {
-      id: '1',
-      label: 'Node',
-      x: 55,
-      y: 255,
-    },
-  ],
-  edges: [
-    {
-      label: 'Label',
-      source: '0',
-      target: '1',
-    },
-  ],
-};
 function App() {
   return (
     <GGEditor className="h-full flex">
@@ -36,11 +12,7 @@ function App() {
         <div style={{height: 'calc(100% - 48px)'}}>
           <Flow data={data} className="bg-gray-100 h-full" />
           <EditableLabel />
-          <CustomNode />
         </div>
-      </div>
-      <div className="w-96 border-l">
-        <NodePanel />
       </div>
     </GGEditor>
   );
